9971e3e8931cf08a1bd8160c585b14aba75442ea
ngyan74 <ngyanchi11@gmail.com>
2025-10-28 13:14:02 -0400

    Add default routes

--- Video_Game_Trends_Analyzer_Project/server/app.js
+++ Video_Game_Trends_Analyzer_Project/server/app.js
@@ -3,4 +3,15 @@ const app = express();
 
 //Routes are here either directly or using a router
 
+//parse json for all routes
+app.use(express.json());
+
+//static files
+app.use(express.static('../client/dist'));
+
+//default 404 page
+app.use((req, res) =>{
+  res.status(404).send('Page not found');
+});
+
 export default app;
