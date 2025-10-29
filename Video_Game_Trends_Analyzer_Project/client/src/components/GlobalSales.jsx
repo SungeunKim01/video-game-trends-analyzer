99fd502ebe56f37c3d521667cd03f4f50377bdcc
ngyan74 <ngyanchi11@gmail.com>
2025-10-28 23:33:57 -0400

    Add stub client component

---
+++ Video_Game_Trends_Analyzer_Project/client/src/components/GlobalSales.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './GlobalSales.css'
+import { useEffect } from 'react';
+
+export default function GlobalSales(){
+    const [data, setData] = useState([]);
+    const [year, setYear] = useState(2006);
+
+    //load top 10 global sales for video games published in 2006 on initial load
+    useEffect(() => {
+        fetch(`/api/sales/global/${year}`).then(resp => {
+            if(resp.ok){
+                return resp.json();
+            } else{
+                throw new Error('status code:' + resp.status)
+            }
+        }).then(json => {
+            setData(json.data);
+        }).catch(error => {
+            console.error(error.message);
+        });
+    }, [])
+
+    console.log('GlobalSales data:', data);
+
+    return(
+        <div>
+            <h2>Displaying Top 10 global sales for video games published in {year}:</h2>
+            <div className='sales-list'>
+                {data.map((game, index) => 
+                    <p key={index}>
+                        {game.name} - {game.global_sales.toFixed(2)} million
+                    </p>
+                )}
+            </div>
+        </div>
+    )
+}
--- Video_Game_Trends_Analyzer_Project/server/app.js
+++ Video_Game_Trends_Analyzer_Project/server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('../client/dist'));
 
 // api routes
-app.use('/sales', salesRouter);
+app.use('/api/sales', salesRouter);
 
 //default 404 page
 app.use((req, res) =>{
