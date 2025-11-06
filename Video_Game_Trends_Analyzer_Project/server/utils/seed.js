24b748b8c1f74493ba8c094f9bd9a20c578bfb3c
Sungeun Kim <sungeun.kim@dawsoncollege.qc.ca>
2025-11-05 21:45:49 -0500

    Re seed Production and my dev database with new trends json file

--- Video_Game_Trends_Analyzer_Project/server/utils/seed.js
+++ Video_Game_Trends_Analyzer_Project/server/utils/seed.js
@@ -5,13 +5,13 @@ import path from 'node:path';
 process.loadEnvFile(path.resolve('.env'));
 
 const vgFilepath = path.resolve('data', 'vgsales.json');
-const trendsFilepath = path.resolve('data', 'trends.json');
+const trendsFilepath = path.resolve('data', 'trends_withregion_unindexed.json');
 
 // Wrap current seeding code in exported function runSeed() 
 // so tests can call it after stubbing fs and db
 export async function runSeed() {
   try {
-    await db.connect(process.env.DEV_DB);
+    await db.connect(process.env.PRO_DB);
     
     // Load the 2 json files
     const rawVGSales = await fs.readFile(vgFilepath, 'utf8');
@@ -20,12 +20,12 @@ export async function runSeed() {
     const trendsData = JSON.parse(rawGoogTrends);
     
     // Insert in Video Game collection
-    await db.setCollection(process.env.DEV_VG_COLLECTION);
+    await db.setCollection(process.env.PRO_VG_COLLECTION);
     const vgSalesNum = await db.createMany(vgsalesData);
     console.log(`Inserted ${vgSalesNum} video games sales entries.`);
     
     // Insert in Google Trends collection
-    await db.setCollection(process.env.DEV_TRENDS_COLLECTION);
+    await db.setCollection(process.env.PRO_TRENDS_COLLECTION);
     const trendsNum = await db.createMany(trendsData);
     console.log(`Inserted ${trendsNum} Google query data.`);
   
