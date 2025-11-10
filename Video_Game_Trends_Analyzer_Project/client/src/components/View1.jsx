3af88a09f11461fc3a15532f273e42efca78b6ec
ngyan74 <ngyanchi11@gmail.com>
2025-11-10 18:28:38 -0500

    Implement SelectFilter in view 1

--- Video_Game_Trends_Analyzer_Project/client/src/components/View1.jsx
+++ Video_Game_Trends_Analyzer_Project/client/src/components/View1.jsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+import SelectFilter from './SelectFilter';
+
 function View1() {
+
+  const [year, setYear] = useState('');
+  const [data, setData] = useState([]);
+
   return (
     <div className="view-div">
-      <h2>Best Selling Games Worldwide</h2>
+      <h2>View 1: Best Global Selling Games </h2>
+      <SelectFilter
+        label="Select Year"
+        //fetch all years from db and populate select
+        fetchURL="/api/sales/years"
+        //if user selects a new year
+        onChange={(newYear) => {
+          //set new year
+          setYear(newYear);
+          //fetch global sales of that year and set data
+          fetch(`/api/sales/global/${newYear}`)
+            .then(res => res.json())
+            .then(json => setData(json.data || []))
+            .catch(err => console.error(err));
+        }}
+      />
+
+      {year && data.length > 0 &&
+        <>
+          {data.map((game, index) => 
+            <p key={index}>
+              {game.name} - {game.global_sales.toFixed(2)} million
+            </p>
+          )}
+        </>
+      }
     </div>
   );
 }
