7164244d9bf2ab181eb40b61ab284cc0c7e67674
ngyan74 <ngyanchi11@gmail.com>
2025-11-18 16:08:14 -0500

    Work on view 2 display

--- Video_Game_Trends_Analyzer_Project/client/src/components/View2.jsx
+++ Video_Game_Trends_Analyzer_Project/client/src/components/View2.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
+import SelectFilter from './SelectFilter';
 import MapChart from './MapChart';
 // import SelectFilter from './SelectFilter';
 
 function View2() {
+
+  const [year, setYear] = useState('');
   const [mapData, setMapData] = useState(null);
+  const [region, setRegion] = useState('');
+  const [games, setGames] = useState([]);
+
+  const [error, setError] = useState('');
 
   // const filterConfig = [
   //   {name: 'country', label: 'country', type: 'text', value: ''}
@@ -34,15 +41,53 @@ function View2() {
     getDefaultGlobalData();
   }, []);
 
-  function handleRegionClick(region) {
-    console.log('Region clicked: ', region);
+  function handleRegionClick(newRegion) {
+    console.log('Region clicked: ', newRegion);
+    setRegion(newRegion);
+    fetch(`/api/sales/region/${newRegion}/${year}`)
+      .then(res => res.json())
+      .then(json => setGames(json))
+      .catch((err) => {
+        setError(err.message);
+        console.error(err);
+      });
   }
 
   return (
     <div className="view-div">
       <h2>Explore Top Games and Google Trends by Country</h2>
+
+      <SelectFilter
+        key="year-filter"
+        label="Select Year"
+        //fetch all years from db and populate select
+        fetchURL="/api/sales/years"
+        value={year}
+        //if user selects a new year
+        onChange={(newYear) => {
+          //set new year
+          setYear(newYear);
+          //fetch global sales / global trends of that year and set data
+          /*fetch(`/api/sales/global/${newYear}`)
+            .then(res => res.json())
+            .then(json => setGames(json))
+            .catch((err) => {
+              setError(err.message);
+              console.error(err);
+            });*/
+        }}
+      />
+
+
+      {/* Error display */}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
       <MapChart mapData={mapData} onRegionClick={handleRegionClick} />
       {/* <FilterForm filterConfig={filterConfig} onSubmit={handleSubmit}/> */}
+
+      {games.forEach(game => {
+        <p>{game}</p>;
+      })}
     </div>
   );
 }
