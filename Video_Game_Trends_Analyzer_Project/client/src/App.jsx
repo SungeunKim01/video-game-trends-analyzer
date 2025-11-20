461effaf50b5ba561b3024f531a923125ced1d99
jenhuang2024 <jennifer.huang@dawsoncollege.qc.ca>
2025-11-20 02:41:05 -0500

    Add title, intro text and its basic styling

--- Video_Game_Trends_Analyzer_Project/client/src/App.jsx
+++ Video_Game_Trends_Analyzer_Project/client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @stylistic/max-len */
 //import { useState } from 'react'
 import './App.css';
 // import GlobalSales from './components/GlobalSales';
@@ -9,9 +10,15 @@ function App() {
 
   return (
     <div>
-      <div className="view-div">
+      <div className="view-div" id="title-div">
         <h1>Video Game Trends Analyzer</h1>
+        <p>Brief description here!!</p>
+        <p>Scroll down</p>
       </div>
+      <section id="intro-section">
+        <h2>Introduction</h2>
+        <p className="intro-text">Introduction text here explaining our app. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </section>
       <View1 />
       <hr />
       <View2 />
