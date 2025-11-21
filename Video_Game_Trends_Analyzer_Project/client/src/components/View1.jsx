d420d4ae8acfd9f8a04aabde375832670c477bcf
jenhuang2024 <jennifer.huang@dawsoncollege.qc.ca>
2025-11-21 14:46:55 -0500

    Restructured View 1; refactor so that each View has its own styling; remove global styling

--- Video_Game_Trends_Analyzer_Project/client/src/components/View1.jsx
+++ Video_Game_Trends_Analyzer_Project/client/src/components/View1.jsx
@@ -9,91 +9,93 @@ function View1() {
   const [category, setCategory] = useState('');
   const [games, setGames] = useState([]);
   const [trends, setTrends] = useState([]);
-
   const [error, setError] = useState('');
 
   return (
-    <div className="view-div" id="view-1">
-      <div className="left-column">
-        <div className="view-title-header">
-          <h2>Global Trends </h2>
-          <SelectFilter
-            key="year-filter"
-            label="Select Year"
-            //fetch all years from db and populate select
-            fetchURL="/api/sales/years"
-            value={year}
-            //if user selects a new year
-            onChange={(newYear) => {
-              //set new year
-              setYear(newYear);
-              //fetch global sales / global trends of that year and set data
-              fetch(`/api/sales/global/${newYear}`)
-                .then(res => res.json())
-                .then(json => setGames(json))
-                .catch((err) => {
-                  setError(err.message);
-                  console.error(err);
-                });
-            }}
-          />
-        </div>
+    <div className="view-div view1-container">
+      <div className="view1-header">
+        <h2> Global Trends </h2>
 
-        <h3>Top 10 Video Games</h3>
-          
-        {/* Error display */}
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-
-        {/* Chart */}
-        <BarChart
-          rows={games}
-        />
-      </div>
-
-      <div className="right-column">
-        {/*context for user */}
-        <p style={{ maxWidth: '820px'}}
-          className="description-text">
-          This bar chart shows the global sales of the top 10 most sold games of the selected year, 
-          in millions. Hover over a bar to see details about a game, including the game sales, 
-          the genre, and the publisher.
-        </p>
-        <p style={{ maxWidth: '820px'}}
-          className="description-text">
-          The side also shows the top 5 most searched terms on Google Trends for that year.
-          Select a specific search category through the dropdown menu. 
-        </p>a
-      
-        {year && games.length > 0 &&
         <SelectFilter
-          key={year}
-          label="Select Category"
-          //fetch global trend categories from db based on year
-          fetchURL={`/api/trends/region/${year}/country/${'GLOBAL'}`}
-          value={category}
-          //if user selects a new category
-          onChange={(newCategory) => {
-            setCategory(newCategory);
-            fetch(`/api/trends/region/${year}/country/${'GLOBAL'}/category/${newCategory}`)
+          key="year-filter"
+          label="Select Year"
+          //fetch all years from db and populate select
+          fetchURL="/api/sales/years"
+          value={year}
+          //if user selects a new year
+          onChange={(newYear) => {
+            //set new year
+            setYear(newYear);
+            //fetch global sales / global trends of that year and set data
+            fetch(`/api/sales/global/${newYear}`)
               .then(res => res.json())
-              .then(json => setTrends(json))
+              .then(json => setGames(json))
               .catch((err) => {
                 setError(err.message);
                 console.error(err);
               });
           }}
         />
-        }
 
-        {category && trends.length > 0 &&
-        <>
-          {trends.map((trend, index) => 
-            <p key={index} className="global-trends-list">
-              {trend.name} - Country: {trend.country} - Rank: {trend.rank} 
-            </p>
-          )}
-        </>
-        }
+        {/*context for user */}
+        <p className="description-text">
+          This bar chart shows the global sales of the top 10 most sold games of the selected year, 
+          in millions. Hover over a bar to see details about a game, including the game sales, 
+          the genre, and the publisher.
+        </p>
+        <p className="description-text">
+          The side also shows the top 5 most searched terms on Google Trends for that year.
+          Select a specific search category through the dropdown menu. 
+        </p>
+      </div>
+
+      <div className="view1-body">
+        <div className="view1-left-column">
+          <h3>Top 10 Video Games</h3>
+          
+          {/* Error display */}
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+
+          {/* Chart */}
+          <BarChart
+            rows={games}
+          />
+        </div>
+
+        <div className="view1-right-column">
+          <h3>Top 5 Google Searches</h3>
+      
+          {year && games.length > 0 &&
+          <SelectFilter
+            key={year}
+            label="Select Category"
+            //fetch global trend categories from db based on year
+            fetchURL={`/api/trends/region/${year}/country/${'GLOBAL'}`}
+            value={category}
+            //if user selects a new category
+            onChange={(newCategory) => {
+              setCategory(newCategory);
+              fetch(`/api/trends/region/${year}/country/${'GLOBAL'}/category/${newCategory}`)
+                .then(res => res.json())
+                .then(json => setTrends(json))
+                .catch((err) => {
+                  setError(err.message);
+                  console.error(err);
+                });
+            }}
+          />
+          }
+
+          {category && trends.length > 0 &&
+          <>
+            {trends.map((trend, index) => 
+              <p key={index} className="global-trends-list">
+                {trend.name} - Country: {trend.country} - Rank: {trend.rank} 
+              </p>
+            )}
+          </>
+          }
+        </div>
       </div>
     </div>
   );
