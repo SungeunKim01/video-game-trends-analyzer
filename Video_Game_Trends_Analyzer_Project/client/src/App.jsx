542256ddc1bd415bb2946b27f75829ac2612d1f8
jenhuang2024 <jennifer.huang@dawsoncollege.qc.ca>
2025-11-20 22:16:54 -0500

    Change font and add down arrow svg

--- Video_Game_Trends_Analyzer_Project/client/src/App.jsx
+++ Video_Game_Trends_Analyzer_Project/client/src/App.jsx
@@ -10,20 +10,18 @@ function App() {
   return (
     <div className="main-container">
       <div className="gradient-bg"/>
-
       <div id="title-div">
         <h1>Video Game Trends Analyzer</h1>
         <p>Keep Scrolling down to see our data!</p>
-        <p>*Scroll down icon here*</p>
+        <svg id="down-arrow" xmlns="http://www.w3.org/2000/svg" height="46px" viewBox="0 -960 960 960" width="40px" fill="#a39f9f"><path d="M480-344 240-584l47.33-47.33L480-438.67l192.67-192.66L720-584 480-344Z"/></svg>
       </div>
       <section id="intro-section">
-        <h2>Introduction</h2>
+        <h2>INTRODUCTION</h2>
         <p className="intro-text">
           Welcome to the Video Game Trends Analyzer! This web app uses a video game sales dataset to determine the most popular games of a certain year, 
           as well as a Google Trends dataset that identifies the most searched google terms around the world per year. Using these datasets, we want to see 
           if there is a correlation between pop culture trends and the most successful video games of each year. We also delve deeper, using different countries
-          to compare what different regions are interested in at a specific point in time, or seeing when certain types of games are most popular. 
-        </p>
+          to compare what different regions are interested in at a specific point in time, or seeing when certain types of games are most popular. </p>
       </section>
       <View1 />
       <View2 />
