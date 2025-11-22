660791bbebd599da756fe2a6f5dc213815120531
ngyan74 <ngyanchi11@gmail.com>
2025-11-21 22:29:27 -0500

    Add indexes when creating db collection

--- Video_Game_Trends_Analyzer_Project/server/utils/seed.js
+++ Video_Game_Trends_Analyzer_Project/server/utils/seed.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import {db} from '../db/db.js';
 import fs from 'node:fs/promises';
 import path from 'node:path';
@@ -23,11 +24,39 @@ export async function runSeed() {
     await db.setCollection(process.env.VG_COLLECTION);
     const vgSalesNum = await db.createMany(vgsalesData);
     console.log(`Inserted ${vgSalesNum} video games sales entries.`);
-    
     // Insert in Google Trends collection
     await db.setCollection(process.env.TRENDS_COLLECTION);
     const trendsNum = await db.createMany(trendsData);
     console.log(`Inserted ${trendsNum} Google query data.`);
+    
+    //get collection obj to use createIndex()
+    const gamesCol = db.db.collection(process.env.VG_COLLECTION);
+    const trendsCol = db.db.collection(process.env.TRENDS_COLLECTION);
+
+    //INDEXES FOR VG SALES FOR PERFORMANCE
+    await gamesCol.createIndex({ Year: 1 });
+    //these are for findTopGamesAllRegionsByYear 
+    //so it doesn't scan through whole collection each time
+    await gamesCol.createIndex({ Year: 1, NA_Sales: 1 });
+    await gamesCol.createIndex({ Year: 1, EU_Sales: 1 });
+    await gamesCol.createIndex({ Year: 1, JP_Sales: 1 });
+    await gamesCol.createIndex({ Year: 1, Other_Sales: 1 });
+    //used by getDistinctByType and getYearlyGameCountByType
+    await gamesCol.createIndex({ Genre: 1 });
+    await gamesCol.createIndex({ Platform: 1 });
+
+    //INDEXES FOR TRENDS
+    await trendsCol.createIndex({ year: 1 });
+    await trendsCol.createIndex({ region: 1 });
+    await trendsCol.createIndex({ country_code: 1 });
+    //used by countriesFromTrends
+    await trendsCol.createIndex({ region: 1, year: 1 });
+    //used by getCategoriesByYearAndCountry
+    await trendsCol.createIndex({ year: 1, country_code: 1 });
+    //used by getTopTrendsByYearAndCategory
+    await trendsCol.createIndex({ year: 1, country_code: 1, category_en: 1 });
+
+    console.log('indexes created successfully');
   
   } catch (e) {
     if (process.env.NODE_ENV === 'test') {
@@ -44,5 +73,4 @@ export async function runSeed() {
     }
   }
 }
-
 // runSeed();
